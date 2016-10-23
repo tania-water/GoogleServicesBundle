abde3a12d3556ae5a7cdf390c65430b80539b44c
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-10-23 11:29:14 +0200

    Enhancement: Add new functions to send silent notifications

--- Service/FirebaseCloudMessaging.php
+++ Service/FirebaseCloudMessaging.php
@@ -33,25 +33,64 @@ class FirebaseCloudMessaging
 
     /**
      * @param Device|Topic $reciver
-     * @param string $notificationTitle
-     * @param string $notificationBody
-     * @param array $notificationData
-     * @param int $deviceNotificationsCount
+     * @param array $data
      * @param string $messagePeriority
      * @return boolean
      */
-    private function sendNotification($reciver, $notificationTitle, $notificationBody, array $notificationData = array(), $deviceNotificationsCount = null, $messagePeriority = 'high')
+    private function sendMessage($reciver, array $data = array(), $messagePeriority = 'high')
     {
         $message = new Message();
         $message->setPriority($messagePeriority);
         $message->addRecipient($reciver);
-        $message->setData($notificationData);
+        if (count($data) > 0) {
+            $message->setData($data);
+        }
+        return $this->fireBaseHTTPClient->send($message)->getStatusCode() == 200 ? true : false;
+    }
+
+    /**
+     * @param string $deviceToken
+     * @param array $notificationData
+     * @param string $messagePeriority
+     * @return boolean
+     */
+    public function sendMessageToDevice($deviceToken, array $notificationData = array(), $messagePeriority = 'high')
+    {
+        return $this->sendMessage(new Device($deviceToken), $notificationData, $messagePeriority);
+    }
+
+    /**
+     * @param string $topicId
+     * @param array $notificationData
+     * @param string $messagePeriority
+     * @return boolean
+     */
+    public function sendMessageToTopic($topicId, array $notificationData = array(), $messagePeriority = 'high')
+    {
+        return $this->sendMessage(new Topic($topicId), $notificationData, $messagePeriority);
+    }
+
+    /**
+     * @param Device|Topic $reciver
+     * @param string $notificationTitle
+     * @param string $notificationBody
+     * @param array $data
+     * @param int $deviceNotificationsCount
+     * @param string $messagePeriority
+     * @return boolean
+     */
+    private function sendNotification($reciver, $notificationTitle, $notificationBody, array $data = array(), $deviceNotificationsCount = null, $messagePeriority = 'high')
+    {
         $notification = new Notification($notificationTitle, $notificationBody);
+        $notification->setPriority($messagePeriority);
+        $notification->addRecipient($reciver);
+        if (count($data) > 0) {
+            $notification->setData($data);
+        }
         if ($deviceNotificationsCount) {
             $notification->setBadge($deviceNotificationsCount);
         }
-        $message->setNotification($notification);
-        return $this->fireBaseHTTPClient->send($message)->getStatusCode() == 200 ? true : false;
+        return $this->fireBaseHTTPClient->send($notification)->getStatusCode() == 200 ? true : false;
     }
 
     /**

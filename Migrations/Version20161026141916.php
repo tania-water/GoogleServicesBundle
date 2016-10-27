f7b2a35461a12bf5b160c1722c0b36db605dd18e
Mahmoud Mostafa <mahmoud.mostafa@ibtikar.net.sa>
2016-10-27 09:23:01 +0200

    Enhancement: Add default time stamp migration

---
+++ Migrations/Version20161026141916.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ibtikar\GoogleServicesBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161026141916 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE device CHANGE created_at created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, CHANGE updated_at updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP');
+        $this->addSql('UPDATE device SET created_at = CURRENT_TIMESTAMP WHERE created_at IS NULL;');
+        $this->addSql('UPDATE device SET updated_at = CURRENT_TIMESTAMP WHERE updated_at IS NULL;');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    }
+}
